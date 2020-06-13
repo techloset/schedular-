511eadb4ff8b97739d0b972e75c61278bac8e785
naveed rana <rana.navee.sarwar@gmail.com>
2020-06-13 16:41:20 +0500

    schedular edits

---
+++ index.d.ts
@@ -0,0 +1,28 @@
+import { Component } from "react";
+import { StyleProp, ViewStyle } from "react-native";
+
+declare module "react-native-24h-timepicker" {
+  export type TimePickerProps = {
+    maxHour?: number;
+    maxMinute?: number;
+    hourInterval?: number;
+    minuteInterval?: number;
+    hourUnit?: string;
+    minuteUnit?: string;
+    selectedHour?: string;
+    selectedMinute?: string;
+    itemStyle: object;
+    textCancel?: string;
+    textConfirm?: string;
+    startFrom?:string;
+    onCancel?: () => void;
+    onConfirm?: () => void;
+  };
+
+  class TimePicker extends Component<TimePickerProps> {
+    open(): void;
+    close(): void;
+  }
+
+  export default TimePicker;
+}
